4015c9480746f47ffc1f38c52a6136a093f78f72
amuslera <arielmuslera@gmail.com>
2022-02-26 15:59:38 -0800

    arregle algunas cosas que no funcionaban del product detail

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -12,17 +12,20 @@ const productController = {
   list: (req, res) => {
     res.render('productList', {title: 'Todos los productos', products: products});
   },
-  detail: (req, res) => {
-		let productoSeleccionado = null;
-		for (i=0;i<products.length;i++) {
-			if(req.params.id == products[i].id) {
-				productoSeleccionado = products[i];
-			}
-		}
-		res.render('productDetail', {title: 'Detalle de producto', productDetail: productoSeleccionado});
-  },
   add: (req, res) => {
     res.render('productCreate', {title: 'Crear Producto Nuevo'});
+  },
+  detail: (req, res) => {
+    let productoSeleccionado = null;
+    for (i=0;i<products.length;i++) {
+    if(req.params.id == products[i].id) {
+      productoSeleccionado = products[i];
+	 		}
+	 	}
+	 	res.render('productDetail', {title: 'Detalle de producto', productDetail: productoSeleccionado});
+  },
+  store: (req, res) =>{
+    res.send('Llegue al controlador')
   },
   edit: (req, res) => {
     res.render('productEdit', {title: 'Editar Producto Nuevo'});
--- src/routes/product.js
+++ src/routes/product.js
@@ -23,15 +23,15 @@ const upload = multer ({storage: storage});
 
 //Rutas de producto
 router.get("/", productController.list);
+router.get('/carrito', productController.carrito);
 router.get("/create", productController.add);
-router.get("/:id", productController.detail);
+router.get("/detail/:id", productController.detail);
 router.get("/:id/edit", productController.edit);
 //router.post('/', productController.???)
 //router.put("/:id", productController.???);
 //router.delete("/:id", productController.???);
 
-router.post("/create", upload.single ('productImage'), productController.edit);
-router.get('/carrito', productController.carrito);
+router.post("/create", upload.single ('productImage'), productController.store);
 
 
 
